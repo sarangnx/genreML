ebbf8a7e15c43958af36d72a478d2e2bc72e1180
Sarang <sarang98divakaran@gmail.com>
2019-03-13 00:11:45 +0530

    Added public interfaces to crop and segment methods

--- spectrograms.py
+++ spectrograms.py
@@ -16,8 +16,8 @@ import config
 
 # Function to crop 30 seconds out of songs
 # inpath - path to raw files directory
-# outpath - path to slices directory
-def cropSongs(inpath,outpath):
+# outpath - path to cropped directory
+def _cropSongs(inpath,outpath):
     files = os.listdir(inpath)
     files = [file for file in files if file.endswith(".mp3")]
 
@@ -28,11 +28,16 @@ def cropSongs(inpath,outpath):
         if not os.path.isdir(outpath):
             # If genre folder doesn't exist create one.
             os.makedirs(outpath)
-        
-        cmd = "ffmpeg -ss 10 -t 30 -i \"{}\" -c copy \"{}\"".format(infile,outfile)
+        # command to trim out 10 seconds from beginning 
+        # and crop the song to 30 seconds
+        cmd = "ffmpeg -y -ss 10 -t 30 -i \"{}\" -c copy \"{}\"".format(infile,outfile)
         subprocess.call(cmd,shell=True)
 
-def sliceSongs(inpath,outpath):
+
+# Slice the songs to 100ms windows
+# inpath - path to cropped files directory
+# outpath - path to slices directory
+def _sliceSongs(inpath,outpath):
     files = os.listdir(inpath)
     files = [file for file in files if file.endswith(".mp3")]
 
@@ -48,18 +53,38 @@ def sliceSongs(inpath,outpath):
             # If genre folder doesn't exist create one.
             os.makedirs(outpath)
         
-        cmd = "ffmpeg -i \"{}\" -f segment -segment_time 0.1 -c copy \"{}%03d.mp3\"".format(infile,outfile)
+        # Command to create segments of duration 0.1s ( = 100ms )
+        cmd = "ffmpeg -y -i \"{}\" -f segment -segment_time 0.1 -c copy \"{}%03d.mp3\"".format(infile,outfile)
         subprocess.call(cmd,shell=True)
 
 
-# def covertToSpectrogram(inpath,outpath):
-#     pylab.axis('off')
-#     pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
+# This is the method that should be called to 
+# crop the songs directly taking the input from
+# "rawDataPath"
+def crop():
+    for genre in os.listdir(config.rawDataPath):
+        infolder = os.path.join(config.rawDataPath,genre)
+        outfolder = os.path.join(config.cropPath,genre)
+        _cropSongs(infolder,outfolder)
 
-#     S = librosa.feature.melspectrogram(y=sig, sr=fs)
-#     librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
-#     pylab.savefig(outpath, bbox_inches=None, pad_inches=0)
-#     pylab.close()
+# This is the method that should be called to 
+# segment/slice the songs directly taking the input from
+# "croppedPath"
+def segment():
+    for genre in os.listdir(config.cropPath):
+        infolder = os.path.join(config.cropPath,genre)
+        outfolder = os.path.join(config.slicesPath,genre)
+        _sliceSongs(infolder,outfolder)
+
+
+def covertToSpectrogram(inpath,outpath):
+    pylab.axis('off')
+    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[])
+
+    S = librosa.feature.melspectrogram(y=sig, sr=fs)
+    librosa.display.specshow(librosa.power_to_db(S, ref=np.max))
+    pylab.savefig(outpath, bbox_inches=None, pad_inches=0)
+    pylab.close()
 
 
 
@@ -70,13 +95,3 @@ def sliceSongs(inpath,outpath):
 #     plot()
 #     count+=1
 
-# for genre in os.listdir(config.rawDataPath):
-#     infolder = os.path.join(config.rawDataPath,genre)
-#     outfolder = os.path.join(config.cropPath,genre)
-#     cropSongs(infolder,outfolder)
-
-
-# for genre in os.listdir(config.cropPath):
-#     infolder = os.path.join(config.cropPath,genre)
-#     outfolder = os.path.join(config.slicesPath,genre)
-#     sliceSongs(infolder,outfolder)

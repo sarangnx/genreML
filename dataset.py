5587df66c0fa3d50cbb18b20d3a9252ecae32cf7
Sarang <sarang98divakaran@gmail.com>
2019-03-13 23:15:09 +0530

    Still working on Dataset loading

--- dataset.py
+++ dataset.py
@@ -16,13 +16,25 @@ from random import shuffle
 from config import datasetPath
 from config import spectrograms
 
+def loadImage(filePath):
+    img = Image.open(filePath)
+    imgData = np.asarray(img) / 255.
+    return imgData
+
 def createDataset(inpath,outpath):
     data = []
     genres = os.listdir(inpath)
     for genre in genres:
-        filenames = os.listdir(os.path.join(inpath + genre))
+        filenames = os.listdir(os.path.join(inpath, genre))
         filenames = [filename for filename in filenames if filename.endswith('.png') ]
         shuffle(filenames)
 
         for filename in filenames:
-            
+            filePath = os.path.join(inpath,genre,filename)
+            imgData = loadImage(filePath)
+            label = [1. if genre == g else 0. for g in genres]
+            data.append((imgData,label))
+    
+    shuffle(data)
+
+    print(type(data))

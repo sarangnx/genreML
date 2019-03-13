f2cf5c699b213fda27405ef56b7a189aeafbb332
Sarang N <sarang98divakaran@gmail.com>
2019-03-13 16:19:06 +0530

    CreateDataset method in progress

--- dataset.py
+++ dataset.py
@@ -11,4 +11,18 @@ import os
 from PIL import Image
 import numpy as np
 import pickle
+from random import shuffle
 
+from config import datasetPath
+from config import spectrograms
+
+def createDataset(inpath,outpath):
+    data = []
+    genres = os.listdir(inpath)
+    for genre in genres:
+        filenames = os.listdir(os.path.join(inpath + genre))
+        filenames = [filename for filename in filenames if filename.endswith('.png') ]
+        shuffle(filenames)
+
+        for filename in filenames:
+            
